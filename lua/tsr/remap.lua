7b85fa3e4e0706b159ba0b64cd320cc4b0672eb7
Tim Raveling <tsraveling@gmail.com>
2025-01-02 12:59:14 -0700

    Tweaked git shortcuts

--- lua/tsr/remap.lua
+++ lua/tsr/remap.lua
@@ -29,8 +29,8 @@ vim.keymap.set("n", "<leader><Esc>", "<cmd>on<CR>")
 
 -- backspace pops out to filesystem
 vim.keymap.set("n", "<BS>", function()
-	vim.cmd("update")
-	vim.cmd("Ex")
+  vim.cmd("update")
+  vim.cmd("Ex")
 end)
 
 -- save shortcut
@@ -43,11 +43,11 @@ vim.keymap.set("n", "<leader>ts", "<cmd>TodoTelescope keywords=STUB<CR>")
 
 -- STUBs in the current project (ie, actual current work designation)
 vim.keymap.set("n", "<leader>]", function()
-	require("todo-comments").jump_next({ keywords = { "STUB" } })
+  require("todo-comments").jump_next({ keywords = { "STUB" } })
 end, { desc = "Next stub" })
 
 vim.keymap.set("n", "<leader>[", function()
-	require("todo-comments").jump_prev({ keywords = { "STUB" } })
+  require("todo-comments").jump_prev({ keywords = { "STUB" } })
 end, { desc = "Next stub" })
 
 --_y to copy to clipboard
@@ -64,34 +64,34 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 --git
 vim.keymap.set("n", "<leader>gs", "<cmd>Git status<CR>")
 vim.keymap.set("n", "<leader>gf", function()
-	vim.cmd("Git fetch origin")
-	vim.notify("Fetched origin.")
+  vim.cmd("Git fetch origin")
+  vim.notify("Fetched origin.")
 end)
-vim.keymap.set("n", "<leader>gg", "<cmd>Git pull origin<CR>")
-vim.keymap.set("n", "<leader>gp", function()
-	vim.cmd("Git push origin")
+vim.keymap.set("n", "<leader>gp", "<cmd>Git pull origin<CR>")
+vim.keymap.set("n", "<leader>gP", function()
+  vim.cmd("Git push origin")
 end)
 
 vim.keymap.set("n", "<leader>gaa", function()
-	vim.cmd('Git add --all')
-	vim.notify("Added all files")
+  vim.cmd('Git add --all')
+  vim.notify("Added all files")
 end, { desc = "Git add all" })
 vim.keymap.set('n', '<leader>gc', function()
-	-- Prompt the user for a commit message
-	vim.ui.input({
-		prompt = "Commit message: ",
-	}, function(commit_message)
-		-- Esc to cancel
-		if commit_message == nil then
-			return
-		end
+  -- Prompt the user for a commit message
+  vim.ui.input({
+    prompt = "Commit message: ",
+  }, function(commit_message)
+    -- Esc to cancel
+    if commit_message == nil then
+      return
+    end
 
-		-- Check if a message was actually entered
-		if commit_message ~= '' then
-			-- Use vim.cmd to run the Git commit command
-			vim.cmd('Git commit -m "' .. commit_message .. '"')
-		else
-			vim.notify("No commit message provided.", "error")
-		end
-	end)
+    -- Check if a message was actually entered
+    if commit_message ~= '' then
+      -- Use vim.cmd to run the Git commit command
+      vim.cmd('Git commit -m "' .. commit_message .. '"')
+    else
+      vim.notify("No commit message provided.", "error")
+    end
+  end)
 end, { desc = 'Git commit with message' })
