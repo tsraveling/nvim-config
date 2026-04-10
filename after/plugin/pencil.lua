9ed9e14039ae8328c71cfbd5843a2c393b255a10
Tim Raveling <tsraveling@proton.me>
2026-04-10 14:01:04 -0600

    updates markdown styling

--- after/plugin/pencil.lua
+++ after/plugin/pencil.lua
@@ -5,9 +5,15 @@ vim.api.nvim_create_autocmd({ 'FileType', 'BufEnter' }, {
   pattern = { 'markdown', 'mkd', 'text', 'skald' },
   callback = function()
     vim.fn['pencil#init']({ wrap = "soft" })
+
+    local yellow = '#fff1ad'
+    local borderYellow = '#020202'
+
     -- Markdown styling
     vim.api.nvim_set_hl(0, '@markup.italic', { italic = true })
-    vim.api.nvim_set_hl(0, '@markup.raw.block.markdown', { bg = '#1d141e', fg = '#b5a5b7', italic = true })
+    vim.api.nvim_set_hl(0, '@markup.raw.block.markdown', { fg = yellow, italic = true })
+    vim.api.nvim_set_hl(0, '@markup.raw.markdown_inline', { fg = yellow, italic = true })
+    vim.api.nvim_set_hl(0, 'RenderMarkdownCode', { bg = borderYellow })
   end,
 })
 
