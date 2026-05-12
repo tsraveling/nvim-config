01b5057af1809688740be83dae025cf0fc8e91a5
Tim Raveling <tsraveling@proton.me>
2026-05-11 23:49:21 -0600

    adds jj escape

--- lua/tsr/graphene.lua
+++ lua/tsr/graphene.lua
@@ -11,4 +11,7 @@ if vim.fn.executable('termux-clipboard-set') == 1 then
     },
     cache_enabled = 0,
   }
+
+  -- Keys-To-Go has no Esc key; jj exits insert mode
+  vim.keymap.set('i', 'jj', '<Esc>')
 end
