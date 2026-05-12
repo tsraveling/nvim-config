b6990d387a45bb536d06798eaef9e1abcb877ee9
Tim Raveling <tsraveling@proton.me>
2026-05-11 23:00:43 -0600

    mobile lsp cleanup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,9 +1,7 @@
 -- Reserve a space in the gutter
--- This will avoid an annoying layout shift in the screen
 vim.opt.signcolumn = 'yes'
 
--- This is where you enable features that only work
--- if there is a language server active in the file
+-- LSP keymaps on attach
 vim.api.nvim_create_autocmd('LspAttach', {
   desc = 'LSP actions',
   callback = function(event)
@@ -11,9 +9,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     vim.keymap.set('n', '<leader>ci', '<cmd>Telescope lsp_implementations<cr>', opts)
     vim.keymap.set('n', '<leader>cd', '<cmd>Telescope lsp_definitions<cr>', opts)
-    vim.keymap.set('n', '<leader>cD', '<cmd>Telescope lsp_declarations<cr>', opts) -- Note: only available in newer versions
+    vim.keymap.set('n', '<leader>cD', '<cmd>Telescope lsp_declarations<cr>', opts)
     vim.keymap.set('n', '<leader>co', '<cmd>Telescope lsp_type_definitions<cr>', opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)     -- Keep diagnostic navigation as-is
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set('n', '<leader>cr', '<cmd>Telescope lsp_references<cr>', opts)
     vim.keymap.set('n', '<leader>cs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
@@ -22,88 +20,22 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
--- Xcodebuild plugin keybindings
-vim.keymap.set('n', '<leader>xb', '<cmd>XcodebuildBuild<cr>', opts)
-vim.keymap.set('n', '<leader>xi', '<cmd>XcodebuildSetup<cr>', opts)
-vim.keymap.set('n', '<leader>xr', '<cmd>XcodebuildBuildRun<cr>', opts)
-vim.keymap.set('n', '<leader>xt', '<cmd>XcodebuildTest<cr>', opts)
-vim.keymap.set('n', '<leader>xd', '<cmd>XcodebuildSelectDevice<cr>', opts)
-vim.keymap.set('n', '<leader>xs', '<cmd>XcodebuildSelectScheme<cr>', opts)
-vim.keymap.set('n', '<leader>xl', '<cmd>XcodebuildToggleLogs<cr>', opts)
-vim.keymap.set('n', '<leader>xc', '<cmd>lua vim.diagnostic.reset()<cr>')
-
-require("mason").setup()
-
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-require("mason-lspconfig").setup({
-  handlers = {
-    -- Default handler for most servers
-    function(server_name)
-      vim.lsp.config(server_name, {
-        capabilities = capabilities,
-      })
-    end,
 
-    -- Custom handler for clangd with your specific configuration
-    clangd = function()
-      vim.lsp.config('clangd', {
-        filetypes = { "c", "cpp", "objc", "objcpp" },
-        cmd = {
-          'clangd',
-          '--background-index',
-          '--clang-tidy',
-          '--log=verbose',
-          '--suggest-missing-includes',
-          '--completion-style=detailed',
-          '--header-insertion=iwyu',
-          '--function-arg-placeholders',
-          '--std=c++20'
-        },
-        settings = {
-          fallbackFlags = { '-std=c++20' },
-        },
-      })
-    end,
-  }
-})
-
--- TypeScript/JavaScript LSP
-vim.lsp.config('ts_ls', {
-  root_markers = { 'package.json', 'tsconfig.json', 'jsconfig.json', '.git' },
-  capabilities = capabilities,
-})
-
--- Skald LSP
-vim.lsp.config("skald_lsp", {
-  cmd = { vim.fn.expand("~/repos/skald/build/skald_lsp") },
-  filetypes = { "skald" },
-  root_markers = { ".git" },
-})
-vim.lsp.enable("skald_lsp")
-
--- Godot LSP
-vim.lsp.config('gdscript', {
-  name = "godot",
-  cmd = vim.lsp.rpc.connect("127.0.0.1", 6005),
-  capabilities = capabilities,
-  on_attach = function(client, bufnr)
-    vim.notify(
-      string.format("Godot LSP connected successfully (client: %s)", client.name),
-      vim.log.levels.INFO,
-      { title = "LSP" }
-    )
-  end,
-})
-
-vim.lsp.config('gdshader_lsp', {})
-
--- Swift LSP configuration
-vim.lsp.config('sourcekit', {
-  cmd = { 'xcrun', 'sourcekit-lsp' },
-  filetypes = { 'swift', 'objective-c', 'objective-cpp' },
-  capabilities = capabilities,
-})
-vim.lsp.enable('sourcekit')
+-- Lua LSP (for editing nvim config)
+if vim.fn.executable('lua-language-server') == 1 then
+  vim.lsp.config('lua_ls', {
+    capabilities = capabilities,
+    settings = {
+      Lua = {
+        diagnostics = { globals = { 'vim' } },
+        workspace = { checkThirdParty = false },
+        telemetry = { enable = false },
+      },
+    },
+  })
+  vim.lsp.enable('lua_ls')
+end
 
 local cmp = require('cmp')
 
@@ -113,13 +45,10 @@ cmp.setup({
   },
   snippet = {
     expand = function(args)
-      -- You need Neovim v0.10 to use vim.snippet
       vim.snippet.expand(args.body)
     end,
   },
-  -- Disable completion in comments
   enabled = function()
-    -- Disable completion in comments using treesitter
     local context = require('cmp.config.context')
     return not context.in_treesitter_capture('comment')
         and not context.in_syntax_group('Comment')
@@ -128,14 +57,12 @@ cmp.setup({
     completion = cmp.config.window.bordered(),
     documentation = cmp.config.window.bordered(),
   },
-  -- TODO: Figure out an abort command that works
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-n>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    -- Add Tab functionality for jumping between placeholders
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<Tab>'] = cmp.mapping(function(fallback)
       if vim.snippet and vim.snippet.active() then
         vim.snippet.jump(1)
@@ -143,8 +70,6 @@ cmp.setup({
         fallback()
       end
     end, { 'i', 's' }),
-
-    -- Add Shift-Tab to jump backwards
     ['<S-Tab>'] = cmp.mapping(function(fallback)
       if vim.snippet and vim.snippet.active() then
         vim.snippet.jump(-1)
@@ -155,21 +80,11 @@ cmp.setup({
   }),
 })
 
+-- Spell on markdown/text/commit
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "markdown", "gitcommit", "text" },
   callback = function()
     vim.opt_local.spell = true
     vim.opt_local.spelllang = "en_us"
   end,
-})
-
--- Typescript organize imports
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "*.tsx", "*.ts", "*.jsx", "*.js" },
-  callback = function()
-    vim.lsp.buf.execute_command({
-      command = "_typescript.organizeImports",
-      arguments = { vim.api.nvim_buf_get_name(0) }
-    })
-  end
 })
